07a70ed2ce383e220bffa6df2a9584d8ac280a94
Khang Nguyen <khangnbk2004@gmai.com>
2026-06-23 11:32:36 +0700

    [UPDATE] Update source code

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,76 +1,66 @@
 #include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Implementation of linked list functions */
-//Initialize the list
+/* Initialize list */
 void list_init(list_t* list) {
-    if (list == NULL) return;
     list->head = NULL;
     list->tail = NULL;
 }
-//Allocate memory for a list
-node_t* list_allocate_node() {
-    node_t* new_node = (node_t*)malloc(sizeof(node_t));
-    if (!new_node) {
-        printf("[list_log] Memory allocation failed\n");
+
+/* Allocate node */
+node_t* list_allocate_node(void) {
+    node_t* node = (node_t*)malloc(sizeof(node_t));
+    if (node == NULL) {
+        printf("[list log] memory allocation failed!\n");
         exit(1);
     }
-    return new_node;
+    return node;
 }
-//Add a new node to the head of the list
+
+/* Insert at head */
 void list_put_head(list_t* list, int data) {
-    node_t* new_node = list_allocate_node();
-
-    new_node->data = data;
-    new_node->next = NULL;
-
-    if (list->head == NULL) {
-        list->head = new_node;
-        list->tail = new_node;
-    }
-    else {
-        new_node->next = list->head;
-        list->head = new_node;
-    }
-}
-//Add a new node to the tail of the list
-void list_put_tail(list_t* list, int data) {
-    node_t* new_node = list_allocate_node();
-
-    new_node->data = data;
-    new_node->next = NULL;
-
+    node_t* node = list_allocate_node();
+    node->data = data;
+    node->next = list->head;
+    list->head = node;
     if (list->tail == NULL) {
-        list->head = new_node;
-        list->tail = new_node;
-    }
-    else {
-        list->tail->next = new_node;
-        list->tail = new_node;
+        list->tail = node;
     }
 }
-//Remove the head node from the list
+
+/* Insert at tail */
+void list_put_tail(list_t* list, int data) {
+    node_t* node = list_allocate_node();
+    node->data = data;
+    node->next = NULL;
+    if (list->tail != NULL) {
+        list->tail->next = node;
+    } else {
+        list->head = node;
+    }
+    list->tail = node;
+}
+
+/* Remove head node */
 void list_remove_head(list_t* list) {
     if (list->head == NULL) {
         printf("[list log] list is empty!\n");
         return;
     }
-    
-    node_t* node_rm = list->head;
-    list->head = node_rm->next;
-    free(node_rm);
-    
+    node_t* rm_node = list->head;
+    list->head = rm_node->next;
     if (list->head == NULL) {
         list->tail = NULL;
     }
+    free(rm_node);
 }
-//Dump the contents of the list
+
+/* Print list */
 void list_dump(list_t* list) {
     printf("\n");
-    node_t* node_dump = list->head;
-    while (node_dump != NULL) {
-        printf("%d -> ", node_dump->data);
-        node_dump = node_dump->next;
+    for (node_t* curr = list->head; curr != NULL; curr = curr->next) {
+        printf("%d -> ", curr->data);
     }
-    printf("NULL\n");
-    printf("\n");
+    printf("NULL\n\n");
 }
--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -4,24 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Define the data structure for a Node*/
+/* Node structure */
 typedef struct node_t {
     int data;
     struct node_t* next;
-}node_t;
+} node_t;
 
-/*Define the data structure for a Linked List*/
+/* List structure */
 typedef struct {
     node_t* head;
     node_t* tail;
-}list_t;
+} list_t;
 
-/*Declare functions for managing and manipulating lists*/
+/* List operations */
 void list_init(list_t* list);
-node_t* list_allocate_node(); 
+node_t* list_allocate_node(void); 
 void list_put_head(list_t* list, int data);  
 void list_put_tail(list_t* list, int data);  
 void list_remove_head(list_t* list);         
 void list_dump(list_t* list); 
 
-#endif //LINKED_LIST_H
+#endif /* LINKED_LIST_H */
